97475764f109b49ef4c88d1c4f908b6f9b50ff22
TARUN KUMAR <122696227+Tarunkumar2407@users.noreply.github.com>
2023-07-28 00:26:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
-// Your Script here.
-let lookup ={
-	'A':'N',
-	'B':'O',
-	'C':'P',
-    'D':'Q',
-    'E':'R',
-    'F':'S',
-    'G':'T',
-    'H':'U',
-    'I':'V',
-    'J':'W',
-    'K':'X',
-    'L':'Y',
-    'M':'Z',
-    'N':'A',
-    'O':'B',
-    'P':'C',
-	'Q':'D',
-	'R':'E',
-	'S':'F',
-	'T':'G',
-	'U':'H',
-    'V':'I',    
-    'W':'J',
-    'X':'K',
-    'Y':'L',
-    'Z':'M',
-	'?':'?',
-	',':','
-};
-function rot13(encodedStr){
-	var words = encodeStr.split(" ");
-	let decryptedWords = [];
-	for (let i = 0; i < words.length; i++) {
-		var encryptedWord = words[i];
-		var decryptedWord = "";
-		for (var j=0 ; j <encryptedWord.length ; j++) {
-			decryptedWord += lookup[encryptedWord.charAt(j)];
-		}
-		decryptedWords.push(decryptedWord);
-	}
-	return decreptedWords.join(" ");
-}
-module.exports = rot13;
+var lookup = {
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q",
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+    "?": "?",
+    ",": ",",
+  };
+  
+  function rot13(encodedStr) {
+    var words = encodedStr.split(" "); 
+    let decodedArr = []; 
+    
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      let decoded_word = "";
+      for (let j = 0; j < word.length; j++) {
+        var char = word.charAt(j);
+        var decoded_char = lookup[char];
+        decoded_word += decoded_char;
+      }
+      decodedArr.push(decoded_word);
+    }
+    return decodedArr.join(" ");
+  }
+ module.exports = rot13;  
+ //  console.log(rot13("V NZ SNEUVA"));
+  
+ 
